4f2dad271bad8b7f9586f9be689a3be36f1a247a
Malachi Cohen <malachicohen@gmail.com>
2025-08-10 13:45:23 +0300

    Add server.js for Hello World client-server app

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
-const http = require('http');
+// server.js
 
-const hostname = '127.0.0.1';
-const port = 3000;
+console.log('Hello World from the server!');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Server!');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Add server-side logic here
