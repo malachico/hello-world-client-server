b1bf77dd4ed5ada910228cfff2a372825dff0ad0
Malachi Cohen <malachicohen@gmail.com>
2025-08-10 13:45:27 +0300

    Add client.py for Hello World client-server application

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 12345        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    request_message = b"Can I have a greeting?"
+    s.sendall(request_message)
+    print(f"Sent: {request_message.decode()}")
+    data = s.recv(1024)
+    print(f"Received: {data.decode()}")
